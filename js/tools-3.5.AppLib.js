3e14b98e74bdc31a5a88864878a771087ddaee50
Cosmas-Klaus <etogacosmas@gmail.com>
2026-04-01 21:24:13 +0100

    test2

---
+++ js/tools-3.5.AppLib.js
@@ -0,0 +1,94 @@
+tools.AppLib = {
+    constantes: {
+        API_BASE_URL: 'http://localhost:8080'
+    },
+    Entities: {
+        Personne: function (data) {
+            tools.Library.Entity.call(this, data);
+
+            this.nom = function () {
+                return this._getDataValue("nom");
+            }
+
+            this.prenom = function () {
+                return this._getDataValue("prenom");
+            }
+
+            this.nomPere = function () {
+                return this._getDataValue("nomPere");
+            }
+
+            this.nomMere = function () {
+                return this._getDataValue("nomMere");
+            }
+
+            this.dateNaiss = function () {
+                return this._getDataValue("dateNaiss");
+            }
+
+            this.lieuNaiss = function () {
+                return this._getDataValue("lieuNaiss");
+            }
+
+            this.sexe = function () {
+                return this._getDataValue("sexe");
+            }
+
+            this.profileImage = function () {
+                return this._getDataValue("profileImage");
+            }
+
+            this.telephone = function () {
+                return this._getDataValue("telephone");
+            }
+        }
+    },
+    ContainerProto: {
+        PersonnesContainer: function (personnes) {
+            this.personnes = personnes;
+            this.matrice = new tools.Library.Stats.Matrice();
+
+            // Representation matricielle de la liste de personnes Tranchehoraire
+            this.getMatriceRepresentation = function () {
+                for (let i = 0; i < this.personnes.length; i++) {
+                    const data = this.personnes[i].getData();
+                    for (const attrib in data) {
+
+                        if (Object.hasOwnProperty.call(data, attrib)) {
+                            const element = data[attrib];
+                            this.matrice.setElement(element, attrib, i);
+                        }
+
+                    }
+                }
+
+                return this.matrice;
+            }
+
+            this.getPersonnes = function () {
+                return this.personnes;
+            }
+        }
+    },
+    Container: {
+        PersonneContainer: function (dao) {
+            tools.Library.Container_mat.call(this, dao);
+            this.table = "Personne";
+
+        }
+    },
+    Models: {
+        PersonneManagers_AJAX: function (api, dao) {
+            tools.Library.Managers_api.call(this, api, dao);
+            this.table = "Personne";
+
+            this.fromContext = function (httpRequest, attribHTTP) {
+                this._requestFetchAll(tools.AppLib.constantes.API_BASE_URL + "/personnes.json", httpRequest, attribHTTP, null);
+            }
+        }
+    },
+    EBackController: function (app, module, action) {
+        tools.Library.BackController.call(this, app, module, action);
+
+    }
+}
---
+++ js/tools-3.5.controller.js
@@ -0,0 +1,20 @@
+tools.Applications.Frontend.Modules.Rh = {
+    DataLoad: {
+        ActionIndex: {
+            loadTest: function (httpResquest) {
+                let managers = httpResquest.getApp().getCurrentController().getManagers();
+                managers.getManagerOf("Personne").fromContext(httpResquest, "personnes");
+            }
+        }
+    },
+    RhController: function (app, module, action) {
+        tools.AppLib.EBackController.call(this, app, module, action);
+
+        this.executeIndex = function (httpRequest) {
+            console.log("index");
+
+        }
+    },
+    Views: {
+    }
+}
