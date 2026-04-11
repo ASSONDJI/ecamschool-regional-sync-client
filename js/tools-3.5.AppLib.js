8ca36ca4b6b45da3adc31fe46a6a352109b89400
ASSONDJI <amalaikaladesse@gmail.com>
2026-04-11 17:22:04 +0100

    feat: implement matrix data loading with AppLib managers and DAO
    - Add User, Establishment, DataMatrix managers in AppLib
    - Add default DAO with uriExist, getElementsIndexUri, updateTable
    - Modify RhController to load data into matrix with separate indices
    - Add POST endpoints in backend for framework compatibility
    - Matrix successfully stores users (lines 0-3), establishments (4-7), data (8-14)
    - getElement works correctly

--- js/tools-3.5.AppLib.js
+++ js/tools-3.5.AppLib.js
@@ -2,93 +2,185 @@ tools.AppLib = {
     constantes: {
         API_BASE_URL: 'http://localhost:8080'
     },
-    Entities: {
-        Personne: function (data) {
-            tools.Library.Entity.call(this, data);
-
-            this.nom = function () {
-                return this._getDataValue("nom");
-            }
-
-            this.prenom = function () {
-                return this._getDataValue("prenom");
-            }
-
-            this.nomPere = function () {
-                return this._getDataValue("nomPere");
-            }
-
-            this.nomMere = function () {
-                return this._getDataValue("nomMere");
-            }
-
-            this.dateNaiss = function () {
-                return this._getDataValue("dateNaiss");
-            }
-
-            this.lieuNaiss = function () {
-                return this._getDataValue("lieuNaiss");
-            }
-
-            this.sexe = function () {
-                return this._getDataValue("sexe");
-            }
-
-            this.profileImage = function () {
-                return this._getDataValue("profileImage");
-            }
-
-            this.telephone = function () {
-                return this._getDataValue("telephone");
+    
+    DAO: {
+        default: {
+            cache: {},
+            
+            uriExist: function(url) {
+                return false;
+            },
+            
+            getElementsIndexUri: function(table, url) {
+                console.log("getElementsIndexUri", table);
+                return this.cache[table] || [];
+            },
+            
+            updateTable: function(uri, table, elements, dataKeysCols) {
+                console.log("updateTable", table, elements.length);
+                this.cache[table] = elements;
+                return true;
+            },
+            
+            getElements: function(table) {
+                return this.cache[table] || [];
             }
         }
     },
-    ContainerProto: {
-        PersonnesContainer: function (personnes) {
-            this.personnes = personnes;
-            this.matrice = new tools.Library.Stats.Matrice();
 
-            // Representation matricielle de la liste de personnes Tranchehoraire
-            this.getMatriceRepresentation = function () {
-                for (let i = 0; i < this.personnes.length; i++) {
-                    const data = this.personnes[i].getData();
-                    for (const attrib in data) {
+  Entities: {
+    Personne: function (data) {
+      tools.Library.Entity.call(this, data);
 
-                        if (Object.hasOwnProperty.call(data, attrib)) {
-                            const element = data[attrib];
-                            this.matrice.setElement(element, attrib, i);
-                        }
+      this.nom = function () {
+        return this._getDataValue("nom");
+      };
 
-                    }
-                }
+      this.prenom = function () {
+        return this._getDataValue("prenom");
+      };
 
-                return this.matrice;
-            }
+      this.nomPere = function () {
+        return this._getDataValue("nomPere");
+      };
 
-            this.getPersonnes = function () {
-                return this.personnes;
-            }
-        }
+      this.nomMere = function () {
+        return this._getDataValue("nomMere");
+      };
+
+      this.dateNaiss = function () {
+        return this._getDataValue("dateNaiss");
+      };
+
+      this.lieuNaiss = function () {
+        return this._getDataValue("lieuNaiss");
+      };
+
+      this.sexe = function () {
+        return this._getDataValue("sexe");
+      };
+
+      this.profileImage = function () {
+        return this._getDataValue("profileImage");
+      };
+
+      this.telephone = function () {
+        return this._getDataValue("telephone");
+      };
     },
-    Container: {
-        PersonneContainer: function (dao) {
-            tools.Library.Container_mat.call(this, dao);
-            this.table = "Personne";
 
-        }
+    User: function (data) {
+      tools.Library.Entity.call(this, data);
     },
-    Models: {
-        PersonneManagers_AJAX: function (api, dao) {
-            tools.Library.Managers_api.call(this, api, dao);
-            this.table = "Personne";
+    Establishment: function (data) {
+      tools.Library.Entity.call(this, data);
+    },
+    DataMatrix: function (data) {
+      tools.Library.Entity.call(this, data);
+    },
+  },
+  ContainerProto: {
+    PersonnesContainer: function (personnes) {
+      this.personnes = personnes;
+      this.matrice = new tools.Library.Stats.Matrice();
 
-            this.fromContext = function (httpRequest, attribHTTP) {
-                this._requestFetchAll(tools.AppLib.constantes.API_BASE_URL + "/personnes.json", httpRequest, attribHTTP, null);
+      // Representation matricielle de la liste de personnes Tranchehoraire
+      this.getMatriceRepresentation = function () {
+        for (let i = 0; i < this.personnes.length; i++) {
+          const data = this.personnes[i].getData();
+          for (const attrib in data) {
+            if (Object.hasOwnProperty.call(data, attrib)) {
+              const element = data[attrib];
+              this.matrice.setElement(element, attrib, i);
             }
+          }
         }
-    },
-    EBackController: function (app, module, action) {
-        tools.Library.BackController.call(this, app, module, action);
 
-    }
-}
+        return this.matrice;
+      };
+
+      this.getPersonnes = function () {
+        return this.personnes;
+      };
+    },
+  },
+  Container: {
+    PersonneContainer: function (dao) {
+      tools.Library.Container_mat.call(this, dao);
+      this.table = "Personne";
+    },
+  },
+  Models: {
+    PersonneManagers_AJAX: function (api, dao) {
+      tools.Library.Managers_api.call(
+        this,
+        api,
+        dao || tools.AppLib.DAO.default,
+      );
+      this.table = "Personne";
+      this.fromContext = function (httpRequest, attribHTTP) {
+        this._requestFetchAll(
+          tools.AppLib.constantes.API_BASE_URL + "/personnes.json",
+          httpRequest,
+          attribHTTP,
+          null,
+        );
+      };
+    },
+
+    UserManagers_AJAX: function (api, dao) {
+      tools.Library.Managers_api.call(
+        this,
+        api,
+        dao || tools.AppLib.DAO.default,
+      );
+      this.table = "User";
+      this.getUsers = function (httpRequest, attribHTTP) {
+        this._requestFetchAll(
+          tools.AppLib.constantes.API_BASE_URL + "/users",
+          httpRequest,
+          attribHTTP,
+          null,
+        );
+      };
+    },
+
+    EstablishmentManagers_AJAX: function (api, dao) {
+      tools.Library.Managers_api.call(
+        this,
+        api,
+        dao || tools.AppLib.DAO.default,
+      );
+      this.table = "Establishment";
+      this.getEstablishments = function (httpRequest, attribHTTP) {
+        this._requestFetchAll(
+          tools.AppLib.constantes.API_BASE_URL + "/establishments",
+          httpRequest,
+          attribHTTP,
+          null,
+        );
+      };
+    },
+
+    DataMatrixManagers_AJAX: function (api, dao) {
+      tools.Library.Managers_api.call(
+        this,
+        api,
+        dao || tools.AppLib.DAO.default,
+      );
+      this.table = "DataMatrix";
+      this.getDataMatrices = function (httpRequest, attribHTTP) {
+        this._requestFetchAll(
+          tools.AppLib.constantes.API_BASE_URL + "/data-matrix",
+          httpRequest,
+          attribHTTP,
+          null,
+        );
+      };
+    },
+  },
+
+  EBackController: function (app, module, action) {
+    tools.Library.BackController.call(this, app, module, action);
+  },
+};
--- js/tools-3.5.controller.js
+++ js/tools-3.5.controller.js
@@ -1,20 +1,100 @@
 tools.Applications.Frontend.Modules.Rh = {
-    DataLoad: {
-        ActionIndex: {
-            loadTest: function (httpResquest) {
-                let managers = httpResquest.getApp().getCurrentController().getManagers();
-                managers.getManagerOf("Personne").fromContext(httpResquest, "personnes");
-            }
-        }
+  DataLoad: {
+    ActionIndex: {
+      loadTest: function (httpRequest) {
+        let managers = httpRequest
+          .getApp()
+          .getCurrentController()
+          .getManagers();
+        managers.getManagerOf("User").getUsers(httpRequest, "users");
+        managers
+          .getManagerOf("Establishment")
+          .getEstablishments(httpRequest, "establishments");
+        managers
+          .getManagerOf("DataMatrix")
+          .getDataMatrices(httpRequest, "dataMatrices");
+      },
     },
-    RhController: function (app, module, action) {
-        tools.AppLib.EBackController.call(this, app, module, action);
+  },
 
-        this.executeIndex = function (httpRequest) {
-            console.log("index");
+  RhController: function (app, module, action) {
+    tools.AppLib.EBackController.call(this, app, module, action);
 
+    this.executeIndex = function (httpRequest) {
+      let users = httpRequest.getAttribute("users");
+      let establishments = httpRequest.getAttribute("establishments");
+      let dataMatrices = httpRequest.getAttribute("dataMatrices");
+
+      let matrice = new tools.Library.Stats.Matrice();
+      let index = 0;
+
+      // Utilisateurs : indices 0 à 99
+      if (users) {
+        for (let i = 0; i < users.length; i++) {
+          const user = users[i];
+          matrice.setElement(user.username, "username", index);
+          matrice.setElement(user.email, "email", index);
+          matrice.setElement(user.role, "role", index);
+          matrice.setElement(user.fullName, "fullName", index);
+          matrice.setElement(user.delegationRegion, "delegationRegion", index);
+          index++;
         }
-    },
-    Views: {
-    }
-}
+        console.log(users.length + " utilisateurs charges");
+      }
+
+      // Établissements : indices 100 à 199
+      if (establishments) {
+        for (let i = 0; i < establishments.length; i++) {
+          const etab = establishments[i];
+          matrice.setElement(etab.code, "code", index);
+          matrice.setElement(etab.name, "name", index);
+          matrice.setElement(etab.city, "city", index);
+          matrice.setElement(etab.delegationRegion, "delegationRegion", index);
+          index++;
+        }
+        console.log(establishments.length + " etablissements charges");
+      }
+
+      // Données : indices 200 à 299
+      if (dataMatrices) {
+        for (let i = 0; i < dataMatrices.length; i++) {
+          const data = dataMatrices[i];
+          matrice.setElement(data.clientKey, "clientKey", index);
+          matrice.setElement(data.syncStatus, "syncStatus", index);
+          matrice.setElement(data.dataType, "dataType", index);
+          index++;
+        }
+        console.log(dataMatrices.length + " donnees chargees");
+      }
+
+      console.log("Matrice:", matrice);
+      console.log("Colonnes:", matrice.getColonnes());
+      console.log("Lignes:", matrice.getLignes());
+
+      window.appMatrix = matrice;
+    };
+
+    this.getManagers = function () {
+      return {
+        getManagerOf: function (entityName) {
+          switch (entityName) {
+            case "Personne":
+              return new tools.AppLib.Models.PersonneManagers_AJAX();
+            case "Enseignement":
+              return new tools.AppLib.Models.EnseignementManagers_AJAX();
+            case "User":
+              return new tools.AppLib.Models.UserManagers_AJAX();
+            case "Establishment":
+              return new tools.AppLib.Models.EstablishmentManagers_AJAX();
+            case "DataMatrix":
+              return new tools.AppLib.Models.DataMatrixManagers_AJAX();
+            default:
+              return null;
+          }
+        },
+      };
+    };
+  },
+
+  Views: {},
+};
