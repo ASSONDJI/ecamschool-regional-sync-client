871764f6946ae0c340a8401e5429e191db91080b
SohTalotsing-ctrl <sohduclair1@gmail.com>
2026-05-02 10:56:48 +0100

    modification en essayant de respecter la structure

--- js/tools-3.5.AppLib.js
+++ js/tools-3.5.AppLib.js
@@ -1,121 +1,163 @@
 tools.AppLib = {
     constantes: {
-        API_BASE_URL: 'http://localhost'
+        API_BASE_URL: 'http://localhost:8080'
     },
+
     Entities: {
-        Personne: function (data) {
-            tools.Library.Entity.call(this, data);
-
-            this.nom = function () {
-                return this._getDataValue("nom");
-            }
-
-            this.prenom = function () {
-                return this._getDataValue("prenom");
-            }
-
-            this.nomPere = function () {
-                return this._getDataValue("nomPere");
-            }
-
-            this.nomMere = function () {
-                return this._getDataValue("nomMere");
-            }
-
-            this.dateNaiss = function () {
-                return this._getDataValue("dateNaiss");
-            }
-
-            this.lieuNaiss = function () {
-                return this._getDataValue("lieuNaiss");
-            }
-
-            this.sexe = function () {
-                return this._getDataValue("sexe");
-            }
-
-            this.profileImage = function () {
-                return this._getDataValue("profileImage");
-            }
-
-            this.telephone = function () {
-                return this._getDataValue("telephone");
-            }
-        },
-        Enseignement: function (data) {
-            tools.Library.Entity.call(this, data);
-
-            this.regime = function () {
-                return this._getDataValue("regime");
-            }
-        },
         User: function (data) {
             tools.Library.Entity.call(this, data);
 
+            this.id = function () {
+                return this._getDataValue("id");
+            }
+
+            this.username = function () {
+                return this._getDataValue("username");
+            }
+
+            this.email = function () {
+                return this._getDataValue("email");
+            }
+
+            this.fullName = function () {
+                return this._getDataValue("fullName");
+            }
+
+            this.role = function () {
+                return this._getDataValue("role");
+            }
+
+            this.delegationRegion = function () {
+                return this._getDataValue("delegationRegion");
+            }
+
+            this.department = function () {
+                return this._getDataValue("department");
+            }
+
+            this.createdAt = function () {
+                return this._getDataValue("createdAt");
+            }
+        },
+
+        Establishment: function (data) {
+            tools.Library.Entity.call(this, data);
+
+            this.id = function () {
+                return this._getDataValue("id");
+            }
+
+            this.code = function () {
+                return this._getDataValue("code");
+            }
+
+            this.name = function () {
+                return this._getDataValue("name");
+            }
+
+            this.city = function () {
+                return this._getDataValue("city");
+            }
+
+            this.department = function () {
+                return this._getDataValue("department");
+            }
+
+            this.delegationRegion = function () {
+                return this._getDataValue("delegationRegion");
+            }
+
+            this.type = function () {
+                return this._getDataValue("type");
+            }
+
+            this.createdAt = function () {
+                return this._getDataValue("createdAt");
+            }
         }
     },
+
     ContainerProto: {
-        PersonnesContainer: function (personnes) {
-            this.personnes = personnes;
+        UsersContainer: function (users) {
+            this.users = users;
             this.matrice = new tools.Library.Stats.Matrice();
 
-            // Representation matricielle de la liste de personnes Tranchehoraire
             this.getMatriceRepresentation = function () {
-                for (let i = 0; i < this.personnes.length; i++) {
-                    const data = this.personnes[i].getData();
-                    for (const attrib in data) {
+                for (let i = 0; i < this.users.length; i++) {
+                    const data = this.users[i].getData();
 
+                    for (const attrib in data) {
                         if (Object.hasOwnProperty.call(data, attrib)) {
                             const element = data[attrib];
                             this.matrice.setElement(element, attrib, i);
                         }
-
                     }
                 }
 
                 return this.matrice;
             }
 
-            this.getPersonnes = function () {
-                return this.personnes;
+            this.getUsers = function () {
+                return this.users;
             }
         }
     },
+
     Container: {
-        PersonneContainer: function (dao) {
+        UserContainer: function (dao) {
             tools.Library.Container_mat.call(this, dao);
-            this.table = "Personne";
+            this.table = "User";
+        },
 
+        EstablishmentContainer: function (dao) {
+            tools.Library.Container_mat.call(this, dao);
+            this.table = "Establishment";
         }
     },
+
     Models: {
-        PersonneManagers_AJAX: function (api, dao) {
-            tools.Library.Managers_api.call(this, api, dao);
-            this.table = "Personne";
-
-            this.fromContext = function (httpRequest, attribHTTP) {
-                this._requestFetchAll(tools.AppLib.constantes.API_BASE_URL + "/personnes.json", httpRequest, attribHTTP, null);
-            }
-        },
-        EnseignementManagers_AJAX: function (api, dao) {
-            tools.Library.Managers_api.call(this, api, dao);
-            this.table = "Enseignement";
-
-            this.getEnseignements = function (httpRequest, attribHTTP) {
-                this._requestFetchAll(tools.AppLib.constantes.API_BASE_URL + "/uds2026/ecamschool-regional-sync-client/uds-server/", httpRequest, attribHTTP, null);
-            }
-        },
         UserManagers_AJAX: function (api, dao) {
             tools.Library.Managers_api.call(this, api, dao);
             this.table = "User";
 
             this.getUsers = function (httpRequest, attribHTTP) {
-                this._requestFetchAll(tools.AppLib.constantes.API_BASE_URL + "/users-lst", httpRequest, attribHTTP, null);
+                const url = tools.AppLib.constantes.API_BASE_URL + "/users";
+
+                console.log(" Appel API USERS:", url);
+
+                // IMPORTANT : on passe directement httpRequest au framework
+                this._requestFetchAll(
+                    url,
+                    httpRequest,
+                    attribHTTP,
+                    null,
+                    []
+                );
             }
+        },
+
+        EstablishmentManagers_AJAX: function (api, dao) {
+            tools.Library.Managers_api.call(this, api, dao);
+            this.table = "Establishment";
+
+            this.getEstablishments = function (httpRequest, attribHTTP) {
+                const url = tools.AppLib.constantes.API_BASE_URL + "/establishments";
+
+                console.log(" Appel API ESTABLISHMENTS:", url);
+
+                // même logique ici
+                this._requestFetchAll(
+                    url,
+                    httpRequest,
+                    attribHTTP,
+                    null,
+                    []
+                );
+            }
+        },
+
+        EBackController: function (app, module, action) {
+            tools.Library.BackController.call(this, app, module, action);
         }
     },
-    EBackController: function (app, module, action) {
-        tools.Library.BackController.call(this, app, module, action);
-
-    }
-}
+};
--- js/tools-3.5.controller.js
+++ js/tools-3.5.controller.js
@@ -1,123 +1,65 @@
 tools.Applications.Frontend.Modules.Rh = {
     DataLoad: {
         ActionIndex: {
-            loadTest: function (httpResquest) {
-                let managers = httpResquest.getApp().getCurrentController().getManagers();
-                managers.getManagerOf("Enseignement").getEnseignements(httpResquest, "enseignements");
-            },
-            // loadTest1: function (httpResquest) {
-            //     let managers = httpResquest.getApp().getCurrentController().getManagers();
-            //     managers.getManagerOf("User").getUsers(httpResquest, "users");
-            // }
+            loadTest: function (httpRequest) {
+                let managers = httpRequest.getApp().getCurrentController().getManagers();
+
+                // Charger USERS depuis backend
+                managers.getManagerOf("User").getUsers(httpRequest, "users");
+
+                // Charger ESTABLISHMENTS depuis backend
+                managers.getManagerOf("Establishment").getEstablishments(httpRequest, "establishments");
+            }
         }
     },
+
     RhController: function (app, module, action) {
-        tools.AppLib.EBackController.call(this, app, module, action);
+        if (tools.AppLib && tools.AppLib.EBackController) {
+            tools.AppLib.EBackController.call(this, app, module, action);
+        } else {
+            console.error("EBackController ou BackController manquant", tools.Library);
+        }
 
         this.executeIndex = function (httpRequest) {
-            let enseignements = httpRequest.getAttribute("enseignements");
-            // let users = httpRequest.getAttribute("users");
-            // Instance de la matrice de représentation des données
-            let matrice = new tools.Library.Stats.Matrice();
 
-            for (let index = 0; index < enseignements.length; index++) {
-                const elt = enseignements[index];
-                matrice.setElement(elt.regime(), "regime", index);
-                // matrice.setElement(elt.getDescription("regime"), "regime", index);
+            let users = httpRequest.getAttribute("users");
+            let establishments = httpRequest.getAttribute("establishments");
+
+            console.log("===== USERS (JSON BACKEND) =====");
+            console.log(users);
+
+            console.log("===== ESTABLISHMENTS (JSON BACKEND) =====");
+            console.log(establishments);
+
+
+            //  Exemple avec matrice pour USERS
+            let matriceUsers = new tools.Library.Stats.Matrice();
+
+            for (let i = 0; i < users.length; i++) {
+                const user = users[i];
+
+                matriceUsers.setElement(user.getData().username, "username", i);
+                matriceUsers.setElement(user.getData().role, "role", i);
             }
 
-            console.log(matrice.getElement("regime", "65"));
-            console.log("after-test");
-
-            matrice.renameCol("regime", "test");
-            console.log(matrice.getElement("test", "65"));
-            console.log(matrice.getElement("regime", "65"));
+            console.log("===== MATRICE USERS =====");
+            console.log(matriceUsers);
 
 
-            console.log(matrice);
+            //  Exemple avec matrice pour ESTABLISHMENTS
+            let matriceEstablishments = new tools.Library.Stats.Matrice();
 
+            for (let i = 0; i < establishments.length; i++) {
+                const est = establishments[i];
+
+                matriceEstablishments.setElement(est.getData().name, "name", i);
+                matriceEstablishments.setElement(est.getData().city, "city", i);
+            }
+
+            console.log("===== MATRICE ESTABLISHMENTS =====");
+            console.log(matriceEstablishments);
         }
-
-        this.executeExportEtsDDESEff = function (httpRequest) {
-            var
-                etablissements = httpRequest.getAttribute("etablissements"),
-                matrice = new tools.Library.Stats.Matrice();
-            // controller.processExportListPersonnelDDES(httpRequest);
-            etablissements.sort(function (a, b) {
-                if (a.nom() < b.nom()) { return -1; }
-                if (a.nom() > b.nom()) { return 1; }
-                return 0;
-            });
-
-            let colsNames = ["global", "solvable", "insolvable", "cas_sociaux"];
-            let colsNamesEts = ["nom", "global", "solvable", "insolvable", "cas_sociaux"];
-
-            for (let i = 0; i < etablissements.length; i++) {
-                const
-                    ets = etablissements[i],
-                    a = ets.getDescription("effectifs")["solvable"],
-                    b = ets.getDescription("effectifs")["insolvable"],
-                    c = ets.getDescription("effectifs")["cas_sociaux"];
-
-                matrice.setElement(ets.nom(), "nom", i);
-                matrice.setElement(ets.cycle(), "cycle", i);
-                matrice.setElement(ets.structure(), "structure", i);
-                matrice.setElement(ets.localite(), "localite", i);
-
-                matrice.setElement(a, "solvable", i);
-                matrice.setElement(b, "insolvable", i);
-                matrice.setElement(c, "cas_sociaux", i);
-                matrice.setElement(a + b + c, "global", i);
-            }
-
-            // matrice.exportXSL("listing global rettraite");
-            let dataConfigXslSchemes = [];
-            let localites = matrice.getColsValuesGroupForColName("localite");
-            // console.log(enseignements);
-            for (const key in localites) {
-                if (Object.hasOwnProperty.call(localites, key)) {
-                    const localite = localites[key];
-                    // console.log(matiere);
-                    const dataConfigXslLocal = {
-                        "config": {
-                            "validators": {
-                                "localite": [
-                                    function (val) {
-                                        return val == localite;
-                                    }
-                                ]
-                            },
-                            "accumulator": function (nbre, somme) {
-                                if (somme == null) {
-                                    return nbre;
-                                }
-
-                                if (nbre == null) {
-                                    return somme;
-                                }
-
-                                return nbre + somme;
-                            },
-                            "label": "SYNTH LOCALITE : " + localite
-                        },
-                        "colonnesShow": colsNamesEts,
-                        "colonnesResume": colsNames,
-                        "title": "data synth : " + localite
-                    };
-
-                    dataConfigXslSchemes = dataConfigXslSchemes.concat(dataConfigXslLocal);
-                }
-            }
-
-            // console.log(dataConfigXslSchemes);
-            let dataConfigXsl = matrice.getConfigGroupExportSchemes(dataConfigXslSchemes, function (resume, result) {
-                // console.log(resume);
-                return resume;
-            }, 'FIN GLOBAL');
-            matrice.exportFileXSL(dataConfigXsl, "general effectif");
-        }
-
     },
-    Views: {
-    }
-}
+
+    Views: {}
+};
